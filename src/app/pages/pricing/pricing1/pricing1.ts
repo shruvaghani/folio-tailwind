f7c76d01bc384ff47ba0a708609b498b950a844b
PriyanshiLakhani <priyanshi.lakhani.it@gmail.com>
2026-05-05 15:31:15 +0530

    wip : pricing

---
+++ src/app/pages/pricing/pricing1/pricing1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing1',
+  imports: [],
+  templateUrl: './pricing1.html',
+  styles: ``,
+})
+export class Pricing1 {
+
+}
---
+++ src/app/pages/pricing/pricing2/pricing2.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing2',
+  imports: [],
+  templateUrl: './pricing2.html',
+  styles: ``,
+})
+export class Pricing2 {
+
+}
