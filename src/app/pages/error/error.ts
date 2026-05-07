e19ef24911aca1ac4148d06c41e847265c0fb073
PriyanshiLakhani <priyanshi.lakhani.it@gmail.com>
2026-05-07 10:05:48 +0530

    fix : ui issue

--- src/app/pages/error/error.ts
+++ src/app/pages/error/error.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Navbar1 } from "@app/components/header/navbar1/navbar1";
+import { Footer3 } from "@app/components/footer/footer3/footer3";
 
 @Component({
   selector: 'app-error',
-  imports: [RouterLink, Navbar1],
+  imports: [RouterLink, Navbar1, Footer3],
   templateUrl: './error.html',
   styles: ``,
 })
